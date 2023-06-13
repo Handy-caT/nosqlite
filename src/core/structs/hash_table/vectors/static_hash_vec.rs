6857c87e7a62606ed01afdf5b2c112632234f923
Maxim Volkov <37216852+Handy-caT@users.noreply.github.com>
2023-06-13 20:35:12 +0200

    added tests for StaticHashVec

--- src/core/structs/hash_table/vectors/static_hash_vec.rs
+++ src/core/structs/hash_table/vectors/static_hash_vec.rs
@@ -27,14 +27,6 @@ impl <V: Default + Eq, const N: u64> StaticHashVec<V, N> {
             max_length: 0,
         }
     }
-
-    pub fn get_vec(&self, index: u64) -> &Vec<V> {
-        &self.data[index as usize]
-    }
-
-    pub fn get_vec_mut(&mut self, index: u64) -> &mut Vec<V> {
-        &mut self.data[index as usize]
-    }
 }
 
 /// Implementation of basic HashVec trait for StaticHashVec
@@ -129,5 +121,148 @@ impl <V: Default + Eq, const N: u64> HashVecInternal<V, N> for StaticHashVec<V, 
         } else {
             Some(&mut self.data[index as usize])
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::core::structs::hash_table::vectors::static_hash_vec::StaticHashVec;
+    use crate::core::structs::hash_table::vectors::hash_vec::{HashVec, HashVecIndexes, HashVecInternal};
+
+    #[test]
+    fn test_static_hash_vec_new() {
+        let hash_vec: StaticHashVec<u64, 8> = StaticHashVec::new();
+
+        assert_eq!(hash_vec.len(), 0);
+        assert_eq!(hash_vec.size, 0);
+        assert_eq!(hash_vec.max_length, 0);
+
+        assert_eq!(hash_vec.data.len(), 8);
+    }
+
+    #[test]
+    fn test_static_hash_vec_push() {
+        let mut hash_vec: StaticHashVec<u64, 8> = StaticHashVec::new();
+
+        hash_vec.push(0, 1);
+        hash_vec.push(0, 2);
+
+        assert_eq!(hash_vec.len(), 2);
+        assert_eq!(hash_vec.size, 2);
+
+        assert_eq!(hash_vec.data[0].len(), 2);
+        assert_eq!(hash_vec.data[0][0], 1);
+        assert_eq!(hash_vec.data[0][1], 2);
+    }
+
+    #[test]
+    fn test_static_hash_vec_have_item() {
+        let mut hash_vec: StaticHashVec<u64, 8> = StaticHashVec::new();
+
+        hash_vec.push(0, 1);
+        hash_vec.push(0, 2);
+
+        assert_eq!(hash_vec.have_item(0, 1), true);
+        assert_eq!(hash_vec.have_item(0, 2), true);
+        assert_eq!(hash_vec.have_item(0, 3), false);
+    }
+
+    #[test]
+    fn test_static_hash_vec_find_item() {
+        let mut hash_vec: StaticHashVec<u64, 8> = StaticHashVec::new();
+
+        hash_vec.push(0, 1);
+        hash_vec.push(0, 2);
+
+        assert_eq!(hash_vec.find_item(0, 1), Some(0));
+        assert_eq!(hash_vec.find_item(0, 2), Some(1));
+        assert_eq!(hash_vec.find_item(0, 3), None);
+    }
+
+    #[test]
+    fn test_static_hash_vec_remove() {
+        let mut hash_vec: StaticHashVec<u64, 8> = StaticHashVec::new();
+
+        hash_vec.push(0, 1);
+        hash_vec.push(0, 2);
+
+        assert_eq!(hash_vec.remove(0, 1), Some(1));
+        assert_eq!(hash_vec.find_item(0, 1), None);
+        assert_eq!(hash_vec.have_item(0, 1), false);
+
+        assert_eq!(hash_vec.remove(0, 2), Some(2));
+        assert_eq!(hash_vec.find_item(0, 2), None);
+        assert_eq!(hash_vec.have_item(0, 2), false);
+
+        assert_eq!(hash_vec.remove(0, 3), None);
+
+        assert_eq!(hash_vec.len(), 0);
+        assert_eq!(hash_vec.size, 0);
+
+        assert_eq!(hash_vec.data[0].len(), 0);
+    }
+
+    #[test]
+    fn test_static_hash_vec_remove_by_index() {
+        let mut hash_vec: StaticHashVec<u64, 8> = StaticHashVec::new();
+
+        hash_vec.push(0, 1);
+        hash_vec.push(0, 2);
+
+        assert_eq!(hash_vec.remove_by_index(0, 0), Some(1));
+        assert_eq!(hash_vec.find_item(0, 1), None);
+        assert_eq!(hash_vec.have_item(0, 1), false);
+
+        assert_eq!(hash_vec.remove_by_index(0, 0), Some(2));
+        assert_eq!(hash_vec.find_item(0, 2), None);
+        assert_eq!(hash_vec.have_item(0, 2), false);
+
+        assert_eq!(hash_vec.remove_by_index(0, 0), None);
+
+        assert_eq!(hash_vec.len(), 0);
+        assert_eq!(hash_vec.size, 0);
+
+        assert_eq!(hash_vec.data[0].len(), 0);
+    }
+
+    #[test]
+    fn test_static_hash_vec_get_by_index() {
+        let mut hash_vec: StaticHashVec<u64, 8> = StaticHashVec::new();
+
+        hash_vec.push(0, 1);
+        hash_vec.push(0, 2);
+
+        assert_eq!(hash_vec.get_by_index(0, 0), Some(&1));
+        assert_eq!(hash_vec.get_by_index(0, 1), Some(&2));
+        assert_eq!(hash_vec.get_by_index(0, 2), None);
+    }
+
+    #[test]
+    fn test_static_hash_vec_get_by_index_mut() {
+        let mut hash_vec: StaticHashVec<u64, 8> = StaticHashVec::new();
+
+        hash_vec.push(0, 1);
+        hash_vec.push(0, 2);
+
+        assert_eq!(hash_vec.get_by_index_mut(0, 0), Some(&mut 1));
+        assert_eq!(hash_vec.get_by_index_mut(0, 1), Some(&mut 2));
+        assert_eq!(hash_vec.get_by_index_mut(0, 2), None);
+    }
+
+    #[test]
+    fn test_static_hash_vec_get_vec() {
+        let mut hash_vec: StaticHashVec<u64, 8> = StaticHashVec::new();
+
+        hash_vec.push(0, 1);
+        hash_vec.push(0, 2);
+
+        let vec = hash_vec.get_vec(0);
+
+        assert_eq!(vec.is_some(), true);
+        assert_eq!(vec.unwrap().len(), 2);
+
+        let vec = hash_vec.get_vec(9);
+
+        assert_eq!(vec.is_some(), false);
     }
 }
