acbacd283f973e614a2f3ed127e996b3ccb32175
Maxim Volkov <37216852+Handy-caT@users.noreply.github.com>
2023-07-31 20:00:54 +0200

    added tests for best fit advisor

--- src/core/advisors/strategies/best_fit_advisor.rs
+++ src/core/advisors/strategies/best_fit_advisor.rs
@@ -2,7 +2,7 @@ use crate::core::advisors::empty_link_registry::EmptyLinkRegistry;
 use crate::core::advisors::strategies::place_advisor_strategy::PlaceAdvisorStrategy;
 use crate::core::link_struct::PageLink;
 use crate::core::structs::tree::object::balanced_tree::balanced_tree::BalancedTree;
-use crate::core::structs::tree::object::tree_object::TreeObjectFind;
+use crate::core::structs::tree::object::tree_object::{TreeObject, TreeObjectFind};
 use crate::core::structs::tree::vectors::tree_vec::{TreeVec, TreeVecIndexes, TreeVecLevels};
 
 /// BestFitAdvisor is a strategy that finds the best fit for a given size.
@@ -48,6 +48,9 @@ impl <'a, V> PlaceAdvisorStrategy for BestFitAdvisor<'a, V>
         let data = self.empty_link_registry.get_data_mut();
         let base_obj = data.get_base_mut();
 
+        if base_obj.len() == 0 {
+            return None;
+        }
         let link = base_obj.find_greater_equal(PageLink::new(0, 0, size as u32));
 
         match link {
@@ -70,5 +73,91 @@ impl <'a, V> PlaceAdvisorStrategy for BestFitAdvisor<'a, V>
 
     fn get_name(&self) -> String {
         "BestFitAdvisor".to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::core::advisors::empty_link_registry::EmptyLinkRegistry;
+    use crate::core::advisors::strategies::best_fit_advisor::BestFitAdvisor;
+    use crate::core::advisors::strategies::place_advisor_strategy::PlaceAdvisorStrategy;
+    use crate::core::link_struct::PageLink;
+    use crate::core::structs::tree::object::balanced_tree::balanced_tree::BalancedTree;
+    use crate::core::structs::tree::object::balanced_tree::decoratable_balanced_tree::DecoratableBalancedTree;
+    use crate::core::structs::tree::object::tree_object::TreeObject;
+    use crate::core::structs::tree::vectors::default_tree_vec::DefaultTreeVec;
+
+    #[test]
+    fn test_best_fit_advisor_new() {
+        let nodes = DefaultTreeVec::<PageLink>::new();
+        let tree = BalancedTree::<PageLink, DefaultTreeVec<PageLink>>::new_with_compare(nodes, PageLink::compare_by_len);
+
+        let dec_tree = DecoratableBalancedTree::<PageLink, DefaultTreeVec<PageLink>, BalancedTree<PageLink, DefaultTreeVec<PageLink>>>::new(tree, PageLink::compare_by_index);
+
+        let mut registry = EmptyLinkRegistry::< DefaultTreeVec<PageLink>, BalancedTree<PageLink, DefaultTreeVec<PageLink>>>::new(dec_tree);
+        let advisor = BestFitAdvisor::new(&mut registry);
+
+        assert_eq!(advisor.get_name(), "BestFitAdvisor".to_string());
+    }
+
+    #[test]
+    fn test_best_fit_advisor_provide_place() {
+        let nodes = DefaultTreeVec::<PageLink>::new();
+        let tree = BalancedTree::<PageLink, DefaultTreeVec<PageLink>>::new_with_compare(nodes, PageLink::compare_by_len);
+
+        let dec_tree = DecoratableBalancedTree::<PageLink, DefaultTreeVec<PageLink>, BalancedTree<PageLink, DefaultTreeVec<PageLink>>>::new(tree, PageLink::compare_by_index);
+
+        let mut registry = EmptyLinkRegistry::< DefaultTreeVec<PageLink>, BalancedTree<PageLink, DefaultTreeVec<PageLink>>>::new(dec_tree);
+
+        registry.add_link(PageLink::new(0, 0, 100));
+        registry.add_link(PageLink::new(0, 100, 200));
+
+        let mut advisor = BestFitAdvisor::new(&mut registry);
+
+        let link = advisor.provide_place(100);
+
+        assert_eq!(link, Some(PageLink::new(0, 0, 100)));
+
+        let link = advisor.provide_place(300);
+
+        assert_eq!(link, None);
+    }
+
+    #[test]
+    fn test_best_fit_advisor_provide_place_with_empty_registry() {
+        let nodes = DefaultTreeVec::<PageLink>::new();
+        let tree = BalancedTree::<PageLink, DefaultTreeVec<PageLink>>::new_with_compare(nodes, PageLink::compare_by_len);
+
+        let dec_tree = DecoratableBalancedTree::<PageLink, DefaultTreeVec<PageLink>, BalancedTree<PageLink, DefaultTreeVec<PageLink>>>::new(tree, PageLink::compare_by_index);
+
+        let mut registry = EmptyLinkRegistry::< DefaultTreeVec<PageLink>, BalancedTree<PageLink, DefaultTreeVec<PageLink>>>::new(dec_tree);
+        let mut advisor = BestFitAdvisor::new(&mut registry);
+
+        let link = advisor.provide_place(100);
+
+        assert_eq!(link, None);
+    }
+
+    #[test]
+    fn test_best_fit_advisor_apply_place() {
+        let nodes = DefaultTreeVec::<PageLink>::new();
+        let tree = BalancedTree::<PageLink, DefaultTreeVec<PageLink>>::new_with_compare(nodes, PageLink::compare_by_len);
+
+        let dec_tree = DecoratableBalancedTree::<PageLink, DefaultTreeVec<PageLink>, BalancedTree<PageLink, DefaultTreeVec<PageLink>>>::new(tree, PageLink::compare_by_index);
+
+        let mut registry = EmptyLinkRegistry::< DefaultTreeVec<PageLink>, BalancedTree<PageLink, DefaultTreeVec<PageLink>>>::new(dec_tree);
+
+        registry.add_link(PageLink::new(0, 0, 100));
+        registry.add_link(PageLink::new(0, 100, 200));
+
+        let mut advisor = BestFitAdvisor::new(&mut registry);
+
+        let link = advisor.provide_place(100);
+
+        assert_eq!(link, Some(PageLink::new(0, 0, 100)));
+
+        advisor.apply_place(&link.unwrap(), 100);
+
+        assert_eq!(registry.get_data().len(), 1);
     }
 }
